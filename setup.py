73d4949407eb0b0d8f31d24d3a93ab4217d28b0a
saadmk11 <saad.mk112@gmail.com>
2020-08-20 19:56:11 +0600

    Added initial setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='django-newsfeed',
+    version='0.0.1',
+    description="""A news curator and newsletter subscription app for django""",
+    author='Maksudul Haque',
+    author_email='saad.mk112@gmail.com',
+    url='https://github.com/saadmk11/django-newsfeed',
+    packages=[
+        'newsfeed',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Django >= 3.0',
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords='django-newsfeed news curator newsletter subscription',
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Framework :: Django :: 3.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
