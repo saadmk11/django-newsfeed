df204d2d34692d159b2cb979b7d1cbf25a167748
saadmk11 <saad.mk112@gmail.com>
2020-08-24 14:14:58 +0600

    tests for apps.py added

---
+++ tests/test_apps.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+from newsfeed.apps import NewsfeedConfig
+
+class NewsfeedConfigTest(TestCase):
+
+    def test_apps(self):
+        self.assertEqual(NewsfeedConfig.name, 'newsfeed')
+        self.assertEqual(apps.get_app_config('newsfeed').name, 'newsfeed')
