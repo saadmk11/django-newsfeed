d9b8c9767145fd07145dcf647cf8166f596e5f31
saadmk11 <saad.mk112@gmail.com>
2020-08-24 17:28:08 +0600

    cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open('README.rst').read()
 
 setup(
     name='django-newsfeed',
-    version='0.1.0',
+    version='0.1.1',
     description="""A news curator and newsletter subscription app for django""",
     long_description=readme,
     author='Maksudul Haque',
@@ -24,8 +24,8 @@ setup(
     zip_safe=False,
     keywords='django-newsfeed news curator newsletter subscription',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django :: 3.1',
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
