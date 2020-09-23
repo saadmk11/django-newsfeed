fc327f4c1081950ff1ff914cb27502a60cc8114e
saadmk11 <saad.mk112@gmail.com>
2020-09-23 14:05:53 +0600

    Release 0.8.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open('README.rst').read()
 
 setup(
     name='django-newsfeed',
-    version='0.8.1',
+    version='0.8.3',
     description="""A news curator and newsletter subscription package for django""",
     long_description=readme,
     long_description_content_type='text/markdown',
