d5614fd6caeefcb625996b7982cc7274b0e58d77
Maksudul Haque <saad.mk112@gmail.com>
2020-11-14 16:25:38 +0600

    Fix tests for python 3.9

--- test_project/settings.py
+++ test_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
